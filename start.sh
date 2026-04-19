37be473640985c37cf3a8ee2b21a491dd773619b
Test <143610555+fish-12345@users.noreply.github.com>
2026-04-19 11:37:08 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -18,4 +18,20 @@ tailscale up \
   --advertise-exit-node \
   --ssh
 
+#!/bin/bash
+curl -fsSL https://tailscale.com/install.sh | sh
+mkdir -p /var/lib/tailscale /var/run/tailscale
+tailscaled \
+  --state=/var/lib/tailscale/tailscaled.state \
+  --socket=/var/run/tailscale/tailscaled.sock \
+  --tun=userspace-networking \
+  --socks5-server=localhost:1055 \
+  --outbound-http-proxy-listen=localhost:1055 &
+sleep 3
+tailscale up \
+  --authkey=${TAILSCALE_AUTHKEY} \
+  --hostname=${TAILSCALE_HOSTNAME:-render-vpn} \
+  --advertise-exit-node \
+  --ssh
 python3 -m http.server 8080 &
+exec sleep infinity
